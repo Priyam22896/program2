0005153c94fbff536d2e716940dc1751c82e0c72
Priyam Patel <Priyam@Priyams-MacBook-Air.local>
2015-04-12 14:58:26 -0400

    Initial Commit

---
+++ program2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  program2
+//
+//  Created by Priyam Patel on 4/12/15.
+//  Copyright (c) 2015 Priyam Patel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
